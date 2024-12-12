94b1f55d99f01396d75a9d63049f757a25f611d1
samigjonov <samigjonov@gmail.com>
2024-12-12 12:06:36 +0500

    feat: Supabase connected to the app

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,20 @@ import { ContactForm } from '../components/contact-form';
 import { Navigation } from '../components/navigation';
 import { Footer } from '../components/footer';
 import { AboutUs } from '@/components/about-us';
+import { createClient } from '@/utils/supabase/server';
 
-export default function Page() {
+export default async function Page() {
+  const supabase = await createClient();
+  const { data: carouselItems } = await supabase.from('carousel_items').select();
   return (
     <div className="min-h-screen bg-background font-sans antialiased">
       <Navigation />
       <main className="flex flex-col items-center justify-center">
-        <section className="w-full">
-          <Carousel />
-        </section>
+        {carouselItems && (
+          <section className="w-full">
+            <Carousel items={carouselItems} />
+          </section>
+        )}
 
         <AboutUs />
         <Features />
